e65127d6ff74d7c816e4f4a438a532b2c89fe5e5
Reis567 <matheusdosreis9@gmail.com>
2023-08-26 18:16:58 -0300

    home url view and template

--- storage_management/storage_management/urls.py
+++ storage_management/storage_management/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from sistema_loja import views
 from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
