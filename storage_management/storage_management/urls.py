0b33e49ec686d7f5eff6128fc4d2c5a12a3f1462
Reis567 <matheusdosreis9@gmail.com>
2023-08-26 20:54:47 -0300

    template_name in urls

--- storage_management/storage_management/urls.py
+++ storage_management/storage_management/urls.py
@@ -23,6 +23,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 ]
