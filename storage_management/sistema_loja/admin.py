0652473c321e2a686c415aa02d19b95ac2137829
Reis567 <matheusdosreis9@gmail.com>
2023-08-26 17:28:00 -0300

    Models verbose and adminregister

--- storage_management/sistema_loja/admin.py
+++ storage_management/sistema_loja/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Departamento, TipoProduto, Vendedor, Produto
 
 # Register your models here.
+
+@admin.register(Departamento)
+class DepartamentoAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+
+@admin.register(TipoProduto)
+class TipoProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'departamento')
+
+@admin.register(Vendedor)
+class VendedorAdmin(admin.ModelAdmin):
+    list_display = ('departamento',)
+    list_filter = ('departamento',)
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'tipo', 'preco', 'quantidade')
+    list_filter = ('tipo__departamento', 'tipo')
--- storage_management/sistema_loja/models.py
+++ storage_management/sistema_loja/models.py
@@ -4,12 +4,20 @@ from django.contrib.auth.models import User
 class Departamento(models.Model):
     nome = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = "Departamento"
+        verbose_name_plural = "Departamentos"
+
     def __str__(self):
         return self.nome
 
 class TipoProduto(models.Model):
     nome = models.CharField(max_length=100)
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Tipo"
+        verbose_name_plural = "Tipos"
 
     def __str__(self):
         return self.nome
@@ -21,6 +29,9 @@ class Vendedor(models.Model):
     # user = models.OneToOneField(User, on_delete=models.CASCADE)
     # Ainda não é possivel colocar o vendedor por nao ter um sistema de login desenvolvido 
 
+    class Meta:
+            verbose_name = "Vendedor"
+            verbose_name_plural = "Vendedores"
     def __str__(self):
         return f"Vendedor do {self.departamento}"
 
@@ -30,6 +41,10 @@ class Produto(models.Model):
     descricao = models.TextField()
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     quantidade = models.PositiveIntegerField()
+
+    class Meta:
+        verbose_name = "Produto"
+        verbose_name_plural = "Produtos"
 
     def __str__(self):
         return self.nome
