58f153c3bfa5e24758487424a0eab01dad5eca37
Reis567 <matheusdosreis9@gmail.com>
2023-08-27 12:57:00 -0300

    products list style

--- storage_management/storage_management/urls.py
+++ storage_management/storage_management/urls.py
@@ -25,4 +25,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('produtos/', views.lista_produtos, name='lista_produtos'),
+    path('produto/<int:produto_id>/', views.detalhes_produto, name='detalhes_produto'),
+
 ]
