3a4771b6c926b5e71f5b5efb53c3974db45b8323
Reis567 <matheusdosreis9@gmail.com>
2023-08-27 12:08:27 -0300

    views and templates of products

--- storage_management/sistema_loja/views.py
+++ storage_management/sistema_loja/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
-
+from .models import Produto
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+
+
+def lista_produtos(request):
+    produtos = Produto.objects.all()
+    return render(request, 'lista_produtos.html', 
+    {
+                'produtos': produtos
+    })
+
+
+def detalhes_produto(request, produto_id):
+    produto = get_object_or_404(Produto, pk=produto_id)
+    return render(request, 'detalhes_produto.html', 
+    {
+        'produto': produto
+    })
