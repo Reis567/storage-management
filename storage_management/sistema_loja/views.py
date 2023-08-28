d4b98817031ea07289f39011a0a52923050b040f
Reis567 <matheusdosreis9@gmail.com>
2023-08-28 08:02:38 -0300

    department filter

--- storage_management/sistema_loja/views.py
+++ storage_management/sistema_loja/views.py
@@ -2,15 +2,27 @@ from django.shortcuts import render
 from .models import Produto
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, Page
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 
-
+@login_required
 def lista_produtos(request):
-    produtos = Produto.objects.all().order_by('tipo__departamento__nome')  # Ordenar por nome do departamento
+    user = request.user
+
+    if user.is_superuser:
+        # Se o usuário é um superusuário (gerência), mostrar todos os produtos
+        produtos = Produto.objects.all().order_by('tipo__departamento__nome')
+    elif user.vendedor:
+        # Se o usuário é um vendedor, filtrar os produtos pelo departamento do vendedor
+        departamento_vendedor = user.vendedor.departamento
+        produtos = Produto.objects.filter(tipo__departamento=departamento_vendedor).order_by('nome')
+    else:
+        produtos = Produto.objects.none()  # Se nenhum vendedor ou gerência, não mostrar produtos
+
 
     paginator = Paginator(produtos, 15)  # Dividir a lista de produtos em páginas de 15 itens cada
     page_number = request.GET.get('page')  # Obter o número da página da query string
