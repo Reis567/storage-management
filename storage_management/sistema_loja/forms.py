e29f1c4c81b9a773a9b14c418e25a0f54d3d82a4
Reis567 <matheusdosreis9@gmail.com>
2023-08-30 08:07:33 -0300

    form to create user

--- storage_management/sistema_loja/forms.py
+++ storage_management/sistema_loja/forms.py
@@ -1,7 +1,20 @@
 from django import forms
 from .models import Produto
+from django.contrib.auth.forms import UserCreationForm
+from .models import Vendedor, Departamento
+from django.contrib.auth.models import User
+
 
 class ProdutoForm(forms.ModelForm):
     class Meta:
         model = Produto
         fields = ['nome', 'foto', 'tipo', 'descricao', 'preco', 'quantidade']
+
+
+class RegistroUsuarioForm(UserCreationForm):
+    departamento = forms.ModelChoiceField(queryset=Departamento.objects.all())
+    foto = forms.ImageField(required=False)
+    
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2', 'departamento', 'foto')
